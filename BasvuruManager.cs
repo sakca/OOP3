6d5e5930a47d2b2ca3115a204477c49b1fadcba3
sarea <sarea@DESKTOP-GC7AIES>
2021-02-20 15:28:21 +0300

    Proje dosyası ekle.

---
+++ BasvuruManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class BasvuruManager
+    {
+        public void BasvuruYap(IKrediManager krediManager ,List<ILoggerService> loggerService)
+        {
+            krediManager.Hesapla();
+            foreach (var log in loggerService)
+            {
+                log.Log();
+             }
+        }
+
+        public void KrediOnBilgilendirmesiYap(List<IKrediManager> krediler)
+        {
+            foreach (var kredi in krediler)
+            {
+                kredi.Hesapla();
+            }
+        }
+    }
+}
